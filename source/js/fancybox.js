4d546044ee2cb12bd69ea36d0cdf170e415f37c2
Yanghao Wang <wyangh90@gmail.com>
2020-12-20 22:06:58 -0500

    Update fancybox.js
    Remove the underline of an inline image in a paragraph

--- source/js/fancybox.js
+++ source/js/fancybox.js
@@ -11,6 +11,7 @@ let fancyTimer = setInterval(function(){
       // $(this).attr("data-fancybox", "gallery"); // if you add 'data-fancybox', img will display after showed
       var element = document.createElement("a");
       $(element).attr("data-fancybox", "gallery");
+      $(element).attr("style", "text-decoration: none; outline: none; border: 0px none transparent;");
       // 判断是否启用了lazyload图片懒加载
       if ($(this).attr("data-original")) {
         $(element).attr("href", $(this).attr("data-original"));
